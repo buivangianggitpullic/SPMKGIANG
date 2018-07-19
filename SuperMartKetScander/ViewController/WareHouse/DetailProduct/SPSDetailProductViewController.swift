053533ec6fa46acda1705c229d6f8c9f41f53c4f
Bui Giang <buigiang@Buis-MacBook-Pro-10.local>
2018-07-20 01:38:17 +0700

    add date selection in add product

--- SuperMartKetScander/ViewController/WareHouse/DetailProduct/SPSDetailProductViewController.swift
+++ SuperMartKetScander/ViewController/WareHouse/DetailProduct/SPSDetailProductViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+enum SPSTypeDate {
+    case NSX
+    case HSD
+    case NN
+}
+
 class SPSDetailProductViewController: SPSBaseViewController {
 
     @IBOutlet private weak var heightContentScrollView: NSLayoutConstraint!
@@ -30,6 +36,9 @@ class SPSDetailProductViewController: SPSBaseViewController {
     
     var codeProduct: String = ""
     let realmManager = RealmManager()
+    var defaultFrame: CGRect = CGRect.zero
+    var strDate: String = ""
+    var typeButton: SPSTypeDate = .NN
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +47,10 @@ class SPSDetailProductViewController: SPSBaseViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+        dateTimeDateView.isHidden = true
+        defaultFrame = dateTimeDateView.frame
+        self.dateTimeDateView.frame = CGRect(x: 0, y: self.view.bounds.height, width: self.view.bounds.width, height: self.view.bounds.height)
+        closeDatePickerView.backgroundColor = UIColor.gray.withAlphaComponent(0.0)
         if UIScreen.main.bounds.height > 600 {
             heightContentScrollView.constant = UIScreen.main.bounds.height - 20
         } else {
@@ -79,16 +92,73 @@ class SPSDetailProductViewController: SPSBaseViewController {
         return prM
     }
     
-    @IBAction func changeDateAction(_ sender: Any) {
+    @IBAction private func changeDateAction(_ sender: Any) {
         let datetime = selectionDateTimePicker.date
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         let myString = formatter.string(from: datetime)
         let yourDate = formatter.date(from: myString)
         formatter.dateFormat = "dd-MM-yyyy"
-        let strDate = formatter.string(from: yourDate!)
+        strDate = formatter.string(from: yourDate!)
     }
     
-    @IBAction func closeButton_Action(_ sender: Any) {
+    @IBAction private func closeButton_Action(_ sender: Any) {
+        dimissDatePicker()
+    }
+    
+    private func showDatePicker() {
+        dateTimeDateView.isHidden = false
+        self.closeDatePickerView.backgroundColor = UIColor.gray.withAlphaComponent(0.0)
+        weak var wSelf = self
+        UIView.animate(withDuration: 1, animations: {
+            guard let mee = wSelf else { return }
+            mee.dateTimeDateView.frame = mee.defaultFrame
+        }) { (complete) in
+            guard let mee = wSelf, complete else { return }
+            mee.closeDatePickerView.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
+        }
+    }
+    
+    func dimissDatePicker() {
+        self.closeDatePickerView.backgroundColor = UIColor.gray.withAlphaComponent(0.0)
+        weak var wSelf = self
+        UIView.animate(withDuration: 1, animations: {
+            guard let mee = wSelf else { return }
+            mee.dateTimeDateView.frame = CGRect(x: 0, y: mee.view.frame.height, width: mee.view.frame.width, height: mee.view.frame.height)
+        }) { (_) in
+            guard let mee = wSelf else { return }
+            mee.dateTimeDateView.isHidden = true
+            switch mee.typeButton {
+            case .NSX:
+                mee.dateOfManuFactireTextField.text = mee.strDate
+            case .HSD:
+                mee.expiryDateTextField.text = mee.strDate
+            default:
+                mee.dateOfEntryTextField.text = mee.strDate
+            }
+        }
+    }
+    
+    @IBAction func selectDateButton_Action(_ sender: Any) {
+        dimissDatePicker()
+    }
+    
+    @IBAction func cancelButton_Action(_ sender: Any) {
+        dimissDatePicker()
+    }
+    
+    @IBAction func hsdDateButton_Action(_ sender: Any) {
+        showDatePicker()
+        typeButton = .HSD
+    }
+    
+    @IBAction func NSXDateButton_Action(_ sender: Any) {
+        showDatePicker()
+        typeButton = .NSX
+    }
+    
+    @IBAction func NNDateButton_Action(_ sender: Any) {
+        showDatePicker()
+        typeButton = .NN
     }
 }
